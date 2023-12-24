d063d5da27dc898ba6c3ef2940e49eba35d39557
Edward B <ebotelloaroca00@gmail.com>
2023-12-23 19:22:56 -0500

    archivo de deteccion de eventos

---
+++ src/navigation.js
@@ -0,0 +1,39 @@
+window.addEventListener('load', navigator, false)
+window.addEventListener('hashchange', navigator, false)
+
+function navigator() {
+    console.log({location});
+
+    if (location.hash.startsWith('#trends')) {
+        trendsPage()
+    } else if (location.hash.startsWith('#search=')) {
+        searchPage()
+    } else if (location.hash.startsWith('#movie=')) {
+        moviePage()
+    } else if (location.hash.startsWith('#category=')) {
+        categoryPage()
+    } else {
+        homePage()
+    }
+}
+
+function homePage() {
+    getTrendingMoviesPreview()
+    getCategoriesPreview()
+}
+
+function trendsPage() {
+    console.log('TRENDS!!');
+}
+
+function searchPage() {
+    console.log('SEARCH!!');
+}
+
+function moviePage() {
+    console.log('MOVIE!!');
+}
+
+function categoryPage() {
+    console.log('CATEGORY!!');
+}
